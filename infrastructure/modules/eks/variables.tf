80f7f18da674372d68332be7eeab51d78931d3f0
Tarun52 <ahujatarun5002@live.com>
2025-10-06 10:43:36 +0530

    Update variables.tf

--- infrastructure/modules/eks/variables.tf
+++ infrastructure/modules/eks/variables.tf
@@ -3,6 +3,11 @@ variable "env" {
   description = "Environment name (e.g., dev, staging, prod)"
 }
 
+variable "region" {
+  type        = string
+  description = "AWS region where EKS will be deployed"
+}
+
 variable "cluster_name" {
   type        = string
   description = "Name of the EKS cluster"
@@ -15,7 +20,7 @@ variable "vpc_id" {
 
 variable "eks_private_subnet_ids" {
   type        = list(string)
-  description = "List of private subnet IDs for EKS nodes"
+  description = "List of private subnet IDs for EKS nodes (ideally 3 from different AZs)"
 }
 
 variable "nodegroups" {
@@ -25,7 +30,7 @@ variable "nodegroups" {
     min_capacity     = number
     instance_types   = list(string)
   }))
-  description = "Map of nodegroups with autoscaling configs and instance types"
+  description = "Map of EKS managed node groups and their scaling configurations"
 }
 
 variable "namespace_names" {
@@ -37,4 +42,30 @@ variable "tags" {
   type        = map(string)
   description = "Tags to apply to all resources"
   default     = {}
+}
+
+variable "ssh_key_name" {
+  type        = string
+  description = "Name of the EC2 SSH key pair to allow SSH access to EKS nodes"
+}
+
+variable "kubeconfig_path" {
+  type        = string
+  description = "Path to the kubeconfig file for interacting with the Kubernetes cluster"
+}
+
+# IRSA (IAM Roles for Service Accounts)
+variable "irsa_namespace" {
+  type        = string
+  description = "Kubernetes namespace where the IRSA-enabled service account will be created"
+}
+
+variable "irsa_service_account_name" {
+  type        = string
+  description = "Name of the Kubernetes service account to bind to the IAM role"
+}
+
+variable "irsa_policy_arn" {
+  type        = string
+  description = "ARN of the IAM policy to attach to the IRSA role (e.g., S3, Route53 access)"
 }
