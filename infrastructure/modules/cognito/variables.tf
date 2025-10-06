01709b7df20326712bf542d64f5c753261af342b
Tarun52 <ahujatarun5002@live.com>
2025-10-06 21:11:06 +0530

    Create variables.tf

---
+++ infrastructure/modules/cognito/variables.tf
@@ -0,0 +1,15 @@
+variable "env" {
+  type = string
+}
+
+variable "callback_urls" {
+  type = list(string)
+}
+
+variable "logout_urls" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
