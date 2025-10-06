13746851949e6d9e336218996b3b4d98acdf6ae6
Tarun52 <ahujatarun5002@live.com>
2025-10-06 21:36:03 +0530

    Update variables.tf

--- infrastructure/modules/route53/variables.tf
+++ infrastructure/modules/route53/variables.tf
@@ -1,15 +1,20 @@
 variable "domain_name" {
-  type = string
+  description = "Root domain name, e.g. example.com"
+  type        = string
 }
 
 variable "subdomain" {
-  type = string
+  description = "Subdomain to create, e.g. api"
+  type        = string
 }
 
-variable "alb_dns" {
-  type = string
+variable "api_gw_domain_target" {
+  description = "API Gateway domain target for Route53 alias"
+  type        = string
 }
 
-variable "alb_zone_id" {
-  type = string
+variable "api_gw_zone_id" {
+  description = "API Gateway's hosted zone ID for alias record"
+  type        = string
 }
+
