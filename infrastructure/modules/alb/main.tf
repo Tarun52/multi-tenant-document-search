a00fe810fe20c3b22af380b63834c67dc49f5849
Tarun52 <ahujatarun5002@live.com>
2025-10-06 18:23:24 +0530

    Update main.tf

--- infrastructure/modules/alb/main.tf
+++ infrastructure/modules/alb/main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "alb_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "Allow HTTP from the internet"
+    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -12,7 +12,6 @@ resource "aws_security_group" "alb_sg" {
   }
 
   egress {
-    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -20,55 +19,7 @@ resource "aws_security_group" "alb_sg" {
   }
 
   tags = {
-    Name = "${var.env}-alb-sg"
-  }
-}
-
-resource "aws_lb" "document_alb" {
-  name               = "${var.env}-document-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = var.public_subnet_ids
-
-  enable_deletion_protection = true
-
-  tags = {
+    Name        = "${var.env}-alb-sg"
     Environment = var.env
-    Component   = "document-alb"
-  }
-}
-
-resource "aws_lb_target_group" "document_tg" {
-  name        = "${var.env}-document-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-
-  health_check {
-    enabled             = true
-    path                = "/health"
-    matcher             = "200-299"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-  }
-
-  tags = {
-    Environment = var.env
-    Component   = "document-target-group"
-  }
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.document_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.document_tg.arn
   }
 }
