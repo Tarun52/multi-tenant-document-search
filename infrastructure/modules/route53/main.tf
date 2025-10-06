043376c2daa245f1f2d6656de424378ef6339b68
Tarun52 <ahujatarun5002@live.com>
2025-10-06 21:31:23 +0530

    Update main.tf

--- infrastructure/modules/route53/main.tf
+++ infrastructure/modules/route53/main.tf
@@ -2,14 +2,13 @@ resource "aws_route53_zone" "document_zone" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "api_record" {
+resource "aws_route53_record" "api_gateway_record" {
   zone_id = aws_route53_zone.document_zone.zone_id
   name    = var.subdomain
   type    = "A"
 
   alias {
-    name                   = var.alb_dns
-    zone_id                = var.alb_zone_id
+    name                   = var.api_gw_domain_target
+    zone_id                = var.api_gw_zone_id
     evaluate_target_health = false
   }
-}
