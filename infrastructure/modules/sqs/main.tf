143c2b07d09e999180e908a7ce72f428d640b26c
Tarun52 <ahujatarun5002@live.com>
2025-10-07 08:37:26 +0530

    Update main.tf

--- infrastructure/modules/sqs/main.tf
+++ infrastructure/modules/sqs/main.tf
@@ -5,3 +5,92 @@ resource "aws_sqs_queue" "document_jobs" {
   delay_seconds              = var.delay_seconds
   tags                       = var.tags
 }
+
+### IAM Role for API Pod (SendMessage)
+resource "aws_iam_role" "api_sqs_role" {
+  name = "${var.env}-api-sqs-role"
+
+  assume_role_policy = data.aws_iam_policy_document.api_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "api_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [var.eks_oidc_provider_arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "${var.eks_oidc_provider_url}:sub"
+      values   = ["system:serviceaccount:${var.api_namespace}:${var.api_service_account}"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "api_sqs_policy" {
+  name = "${var.env}-api-sqs-policy"
+  role = aws_iam_role.api_sqs_role.id
+
+  policy = data.aws_iam_policy_document.api_policy.json
+}
+
+data "aws_iam_policy_document" "api_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:SendMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl"
+    ]
+    resources = [aws_sqs_queue.document_jobs.arn]
+  }
+}
+
+### IAM Role for Worker Pod (Receive/Delete)
+resource "aws_iam_role" "worker_sqs_role" {
+  name = "${var.env}-worker-sqs-role"
+
+  assume_role_policy = data.aws_iam_policy_document.worker_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "worker_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [var.eks_oidc_provider_arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "${var.eks_oidc_provider_url}:sub"
+      values   = ["system:serviceaccount:${var.worker_namespace}:${var.worker_service_account}"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "worker_sqs_policy" {
+  name = "${var.env}-worker-sqs-policy"
+  role = aws_iam_role.worker_sqs_role.id
+
+  policy = data.aws_iam_policy_document.worker_policy.json
+}
+
+data "aws_iam_policy_document" "worker_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:ChangeMessageVisibility",
+      "sqs:GetQueueAttributes"
+    ]
+    resources = [aws_sqs_queue.document_jobs.arn]
+  }
+}
